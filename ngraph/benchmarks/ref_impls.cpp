02a7689743b2433d30fd527d4c0e5a1f24d4450c
jdanieck <jozef.daniecki@intel.com>
2020-12-16 17:21:53 +0100

    Add optimization prevention.

--- ngraph/benchmarks/ref_impls.cpp
+++ ngraph/benchmarks/ref_impls.cpp
@@ -14,7 +14,9 @@ static void range_ref_impl(benchmark::State& state)
 
     for (auto _ : state)
     {
+        benchmark::DoNotOptimize(out.data());
         ngraph::runtime::reference::range(&start, &step, out_size, out.data());
+        benchmark::ClobberMemory();
     }
 }
 BENCHMARK(range_ref_impl)->Unit(benchmark::kMicrosecond)->Range(1000, 10000000);
@@ -24,11 +26,13 @@ static void range_std_iota(benchmark::State& state)
     const int start = 0;
     const int step = 1;
     const size_t out_size = state.range(0);
-    std::vector<int> out(state.range(0));
+    std::vector<int> out(out_size);
 
     for (auto _ : state)
     {
+        benchmark::DoNotOptimize(out.data());
         std::iota(out.begin(), out.end(), start);
+        benchmark::ClobberMemory();
     }
 }
 BENCHMARK(range_std_iota)->Unit(benchmark::kMicrosecond)->Range(1000, 10000000);
