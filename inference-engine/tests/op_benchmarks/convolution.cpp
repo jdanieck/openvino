a986d00547235507d4ba7bfb4a5008d3c01511f6
jdanieck <jozef.daniecki@intel.com>
2021-06-21 17:08:58 +0200

    Refactored tests to use InferenceEngine API.

--- inference-engine/tests/op_benchmarks/convolution.cpp
+++ inference-engine/tests/op_benchmarks/convolution.cpp
@@ -1,249 +1,149 @@
+#include "ngraph/runtime/reference/convolution.hpp"
+
+#include <benchmark/benchmark.h>
+
+#include <ie_core.hpp>
 #include <numeric>
 #include <vector>
 
-#include <benchmark/benchmark.h>
 #include "ngraph/ngraph.hpp"
-#include "ngraph/runtime/reference/convolution.hpp"
 
 using namespace ngraph;
 
+namespace {
+enum class DeviceType { CPU, TEMPLATE };
+template <typename T>
+struct BenchmarkConfig {
+    Strides strides;
+    CoordinateDiff pads_begin;
+    CoordinateDiff pads_end;
+    Strides dilations;
+    Shape inputs_shape;
+    std::vector<T> inputs;
+    Shape filter_shape;
+    std::vector<T> filters;
+    Shape outputs_shape;
+    std::vector<T> outputs;
+    op::PadType auto_pad;
+};
 
+template <typename T>
+std::shared_ptr<ngraph::Function> getFunction(const BenchmarkConfig<T>& cfg) {
+    auto inputs_param =
+        std::make_shared<op::Parameter>(element::f32, cfg.inputs_shape);
+    inputs_param->set_friendly_name("inputs");
+    auto filters_param =
+        std::make_shared<op::Parameter>(element::f32, cfg.filter_shape);
+    filters_param->set_friendly_name("filters");
+    auto conv = std::make_shared<op::v1::Convolution>(
+        inputs_param, filters_param, cfg.strides, cfg.pads_begin, cfg.pads_end,
+        cfg.dilations, cfg.auto_pad);
+    conv->set_friendly_name("convolution");
+    return std::make_shared<Function>(
+        conv, ParameterVector{inputs_param, filters_param});
+}
 
-static void convolution_1D_small_data(benchmark::State& state)
-{
-    const Strides strides{1};
-    const CoordinateDiff padding{0};
-    const Strides dilations{1};
+BenchmarkConfig<float> getConfig() {
+    using T = float;
 
-    const Shape inputs_shape{1, 1, 6};
-    std::vector<float> inputs(shape_size(inputs_shape));
-    std::iota(inputs.begin(), inputs.end(), 0);
+    BenchmarkConfig<T> cfg;
+    cfg.strides = Strides{1, 1};
+    cfg.pads_begin = cfg.pads_end = CoordinateDiff{0, 0};
+    cfg.dilations = Strides{1, 1};
+    cfg.auto_pad = op::PadType::EXPLICIT;
 
-    const Shape filter_shape{1, 1, 3};
-    const std::vector<float> filters{2.0f, 0.0f, 1.0f};
+    cfg.inputs_shape = Shape{1, 1, 4, 4};
+    cfg.inputs = std::vector<T>{1.0f, 3.0f, 5.0f, 7.0f, 7.0f, 5.0f, 3.0f, 1.0f,
+                                2.0f, 4.0f, 6.0f, 8.0f, 8.0f, 6.0f, 4.0f, 2.0f};
+    cfg.filter_shape = Shape{1, 1, 3, 3};
+    cfg.filters =
+        std::vector<T>{1.0f, 2.0f, 3.0f, 0.0f, 1.0f, 0.0f, 3.0f, 2.0f, 1.0f};
 
-    const Shape outputs_shape{1, 1, 4};
-    std::vector<float> out(shape_size(outputs_shape));
-    
-    for (auto _ : state)
-    {
+    cfg.outputs_shape = Shape{1, 1, 2, 2};
+    cfg.outputs = std::vector<T>{47.0f, 69.0f, 70.0f, 48.0f};
+    return cfg;
+}
+}  // namespace
+
+static void convolution_2D_reference(benchmark::State& state) {
+    // setup
+    const auto cfg = getConfig();
+    std::vector<float> out(shape_size(cfg.outputs_shape));
+
+    // benchmark
+    for (auto _ : state) {
         benchmark::DoNotOptimize(out.data());
-        runtime::reference::convolution(inputs.data(),
-                                        filters.data(),
-                                        out.data(),
-                                        inputs_shape,
-                                        filter_shape,
-                                        outputs_shape,
-                                        strides,
-                                        dilations,
-                                        padding,
-                                        padding,
-                                        strides);
+        runtime::reference::convolution(
+            cfg.inputs.data(), cfg.filters.data(), out.data(), cfg.inputs_shape,
+            cfg.filter_shape, cfg.outputs_shape, cfg.strides, cfg.dilations,
+            cfg.pads_begin, cfg.pads_end, cfg.strides);
 
         benchmark::ClobberMemory();
     }
-}
-BENCHMARK(convolution_1D_small_data)->Unit(benchmark::kNanosecond);
 
-static void convolution_1D_large_data(benchmark::State& state)
-{
-    const Strides strides{1};
-    const CoordinateDiff padding{0};
-    const Strides dilations{1};
-
-    const Shape inputs_shape{10, 1, 100};
-    std::vector<float> inputs(shape_size(inputs_shape));
-    std::iota(inputs.begin(), inputs.end(), 0);
-
-    const Shape filter_shape{1, 1, 3};
-    const std::vector<float> filters{2.0f, 0.0f, 1.0f};
-
-    const Shape outputs_shape{10, 1, 98};
-    std::vector<float> out(shape_size(outputs_shape));
-
-    for (auto _ : state)
-    {
-        benchmark::DoNotOptimize(out.data());
-        runtime::reference::convolution(inputs.data(),
-                                        filters.data(),
-                                        out.data(),
-                                        inputs_shape,
-                                        filter_shape,
-                                        outputs_shape,
-                                        strides,
-                                        dilations,
-                                        padding,
-                                        padding,
-                                        strides);
-
-        benchmark::ClobberMemory();
+    // sanity check: generated output should be the same as expected
+    for (size_t i = 0; i < cfg.outputs.size(); i++) {
+        if (cfg.outputs[i] != out[i]) {
+            std::cout << "convolution_2D_reference failed!" << std::endl;
+            exit(-1);
+        }
     }
 }
-BENCHMARK(convolution_1D_large_data)->Unit(benchmark::kNanosecond);
+BENCHMARK(convolution_2D_reference);
 
-static void convolution_2D_small_data(benchmark::State& state)
-{
-    const Strides strides{1, 1};
-    const CoordinateDiff padding{0, 0};
-    const Strides dilations{1, 1};
+template <DeviceType dev>
+static void convolution_2D_plugin(benchmark::State& state) {
+    using namespace InferenceEngine;
 
-    const Shape inputs_shape{1, 1, 4, 4};
-    std::vector<float> inputs(shape_size(inputs_shape));
-    std::iota(inputs.begin(), inputs.end(), 0);
+    // setup
+    const auto cfg = getConfig();
+    const auto f = getFunction(cfg);
 
-    const Shape filter_shape{1, 1, 3, 3};
-    const std::vector<float> filters{1.0f, 2.0f, 3.0f,
-                                    0.0f, 1.0f, 0.0f,
-                                    3.0f, 2.0f, 1.0f};
+    const auto network = InferenceEngine::CNNNetwork(f);
+    InferenceEngine::Core ie;
+    auto exe_network = dev == DeviceType::TEMPLATE
+                           ? ie.LoadNetwork(network, "TEMPLATE")
+                           : ie.LoadNetwork(network, "CPU");
+    auto inference_req = exe_network.CreateInferRequest();
 
-
-    const Shape outputs_shape{1, 1, 2, 2};
-    std::vector<float> out(shape_size(outputs_shape));
-
-    for (auto _ : state)
     {
-        benchmark::DoNotOptimize(out.data());
-        runtime::reference::convolution(inputs.data(),
-                                        filters.data(),
-                                        out.data(),
-                                        inputs_shape,
-                                        filter_shape,
-                                        outputs_shape,
-                                        strides,
-                                        dilations,
-                                        padding,
-                                        padding,
-                                        strides);
+        const auto inputs_info = network.getInputsInfo();
 
-        benchmark::ClobberMemory();
+        const auto in = InferenceEngine::make_shared_blob<float>(
+            {InferenceEngine::Precision::FP32, cfg.inputs_shape,
+             InferenceEngine::Layout::NCHW},
+            const_cast<float*>(cfg.inputs.data()), cfg.inputs.size());
+        inference_req.SetBlob("inputs", in);
+
+        const auto filters = InferenceEngine::make_shared_blob<float>(
+            {InferenceEngine::Precision::FP32, cfg.filter_shape,
+             InferenceEngine::Layout::NCHW},
+            const_cast<float*>(cfg.filters.data()), cfg.filters.size());
+        inference_req.SetBlob("filters", filters);
+    }
+
+    {
+        DataPtr output_info = network.getOutputsInfo()["convolution"];
+        output_info->setPrecision(InferenceEngine::Precision::FP32);
+    }
+
+    // benchmark
+    for (auto _ : state) {
+        inference_req.Infer();
+    }
+
+    // sanity check: generated output should be the same as expected
+    auto output_blob = inference_req.GetBlob("convolution");
+    auto const memLocker = output_blob->cbuffer();  // use const memory locker
+    const float* out = memLocker.as<const float*>();
+
+    for (size_t i = 0; i < cfg.outputs.size(); i++) {
+        if (cfg.outputs[i] != out[i]) {
+            std::cout << "convolution_2D_template_plugin failed!" << std::endl;
+            exit(-1);
+        }
     }
 }
-BENCHMARK(convolution_2D_small_data)->Unit(benchmark::kNanosecond);
 
-static void convolution_2D_large_data(benchmark::State& state)
-{
-    const Strides strides{1, 1};
-    const CoordinateDiff padding{0, 0};
-    const Strides dilations{1, 1};
-
-    const Shape inputs_shape{10, 1, 100, 100};
-    std::vector<float> inputs(shape_size(inputs_shape));
-    std::iota(inputs.begin(), inputs.end(), 0);
-
-    const Shape filter_shape{1, 1, 3, 3};
-    const std::vector<float> filters{1.0f, 2.0f, 3.0f,
-                                    0.0f, 1.0f, 0.0f,
-                                    3.0f, 2.0f, 1.0f};
-
-    const Shape outputs_shape{10, 1, 98, 98};
-    std::vector<float> out(shape_size(outputs_shape));
-
-    for (auto _ : state)
-    {
-        benchmark::DoNotOptimize(out.data());
-        runtime::reference::convolution(inputs.data(),
-                                        filters.data(),
-                                        out.data(),
-                                        inputs_shape,
-                                        filter_shape,
-                                        outputs_shape,
-                                        strides,
-                                        dilations,
-                                        padding,
-                                        padding,
-                                        strides);
-
-        benchmark::ClobberMemory();
-    }
-}
-BENCHMARK(convolution_2D_large_data)->Unit(benchmark::kNanosecond);
-
-static void convolution_3D_small_data(benchmark::State& state)
-{
-    const Strides strides{1, 1, 1};
-    const CoordinateDiff padding{0, 0, 0};
-    const Strides dilations{1, 1, 1};
-
-    const Shape inputs_shape{1, 1, 4, 4, 4};
-    std::vector<float> inputs(shape_size(inputs_shape));
-    std::iota(inputs.begin(), inputs.end(), 0);
-
-    const Shape filter_shape{1, 1, 3, 3, 3};
-     const std::vector<float> filters{                                    
-                                    1.0f, 2.0f, 3.0f,
-                                    0.0f, 1.0f, 0.0f,
-                                    2.0f, 1.0f, 2.0f,                                    
-                                    1.0f, 2.0f, 3.0f,
-                                    0.0f, 1.0f, 0.0f,
-                                    2.0f, 1.0f, 2.0f,                                    
-                                    1.0f, 2.0f, 3.0f,
-                                    0.0f, 1.0f, 0.0f,
-                                    2.0f, 1.0f, 2.0f};
-
-    const Shape outputs_shape{1, 1, 2, 2, 2};
-    std::vector<float> out(shape_size(outputs_shape));
-
-    for (auto _ : state)
-    {
-        benchmark::DoNotOptimize(out.data());
-        runtime::reference::convolution(inputs.data(),
-                                        filters.data(),
-                                        out.data(),
-                                        inputs_shape,
-                                        filter_shape,
-                                        outputs_shape,
-                                        strides,
-                                        dilations,
-                                        padding,
-                                        padding,
-                                        strides);
-
-        benchmark::ClobberMemory();
-    }
-}
-BENCHMARK(convolution_3D_small_data)->Unit(benchmark::kNanosecond);
-
-static void convolution_3D_large_data(benchmark::State& state)
-{
-    const Strides strides{1, 1, 1};
-    const CoordinateDiff padding{0, 0, 0};
-    const Strides dilations{1, 1, 1};
-
-    const Shape inputs_shape{10, 1, 100, 100, 100};
-    std::vector<float> inputs(shape_size(inputs_shape));
-    std::iota(inputs.begin(), inputs.end(), 0);
-
-    const Shape filter_shape{1, 1, 3, 3, 3};
-     const std::vector<float> filters{                                    
-                                    1.0f, 2.0f, 3.0f,
-                                    0.0f, 1.0f, 0.0f,
-                                    2.0f, 1.0f, 2.0f,                                    
-                                    1.0f, 2.0f, 3.0f,
-                                    0.0f, 1.0f, 0.0f,
-                                    2.0f, 1.0f, 2.0f,                                    
-                                    1.0f, 2.0f, 3.0f,
-                                    0.0f, 1.0f, 0.0f,
-                                    2.0f, 1.0f, 2.0f};
-
-    const Shape outputs_shape{10, 1, 98, 98, 98};
-    std::vector<float> out(shape_size(outputs_shape));
-
-    for (auto _ : state)
-    {
-        benchmark::DoNotOptimize(out.data());
-        runtime::reference::convolution(inputs.data(),
-                                        filters.data(),
-                                        out.data(),
-                                        inputs_shape,
-                                        filter_shape,
-                                        outputs_shape,
-                                        strides,
-                                        dilations,
-                                        padding,
-                                        padding,
-                                        strides);
-
-        benchmark::ClobberMemory();
-    }
-}
-BENCHMARK(convolution_3D_large_data)->Unit(benchmark::kNanosecond);
+BENCHMARK_TEMPLATE(convolution_2D_plugin, DeviceType::TEMPLATE);
+BENCHMARK_TEMPLATE(convolution_2D_plugin, DeviceType::CPU);
